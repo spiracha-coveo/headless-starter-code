7dac207166e915941f3bbe9336fa255de2224b08
Sym Piracha <spiracha@coveo.com>
2023-03-09 15:50:20 -0500

    remove changed searchbox"

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -10,45 +10,20 @@ interface SearchBoxProps {
 const SearchBox: React.FC<SearchBoxProps> = (props) => {
     const {controller} = props
     const [state, setState] = useState(controller.state);
-    const [focused, setFocused] = useState(false);
 
     useEffect(
       () => controller.subscribe(() => setState(controller.state)),
       [controller]
     );
     return (
-      
-      <div className="search-box">
-        <input
+      <div className='search-box'>
+      <input
+          placeholder='Search in Barca...'
           value={state.value}
           onChange={(e) => controller.updateText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              controller.submit();
-            } else if (e.key === 'Escape') {
-              controller.clear();
-              (e.target as HTMLInputElement).blur();
-            }
-          }}
-          onFocus={() => setFocused(true)}
-          onBlur={() => setFocused(false)}
-        />
-        {focused && state.suggestions.length > 0 && (
-          <div className='search-queries'>
-            {state.suggestions.map((suggestion) => {
-              return (
-                <p
-                  key={suggestion.rawValue}
-                  onMouseDown={(e) => e.preventDefault()}
-                  onClick={() => controller.selectSuggestion(suggestion.rawValue)}
-                  dangerouslySetInnerHTML={{__html: suggestion.highlightedValue}}
-                ></p>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    
+          onKeyDown={(e) => e.key === 'Enter' && controller.submit()}
+          />
+  </div>
     );
   };
 
